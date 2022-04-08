2d899304914a0adc49db3e8c68f9ecfb56f7a90f
Praan26 <arpanap102@gmail.com>
2022-04-08 22:34:11 +0530

    [Arpan] Add: Equality of two lines has been added

--- LineComparision/LineComparision/Program.cs
+++ LineComparision/LineComparision/Program.cs
@@ -1,3 +1,16 @@
 ﻿using LineComparision;
-Comparision linecomparision = new Comparision(1, 2, 3, 4);
-linecomparision.CalculateLength();
+Comparision comparision = new Comparision(1, 2, 3, 4);
+comparision.CalculateLength();
+Comparision line1 = new Comparision(1, 2, 3, 4);
+line1.CalculateLength();
+Comparision line2 = new Comparision(1, 2, 3, 4);
+line2.CalculateLength();
+
+if (line1.Equals(line2))
+{
+    Console.WriteLine("Both lines are equal");
+}
+else
+{
+    Console.WriteLine("Both lines are not equal");
+}
